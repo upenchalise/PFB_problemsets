0517f61d26553460447764c96a91d4f2ad4879c1
pfb2024 <pfb2024@info19.cshl.edu>
2024-10-15 12:20:38 -0400

    modified aboutme.py

--- aboutme.py
+++ aboutme.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
-print ("My name: Upendra")
+name = "Upendra"
+color = "Blue"
+activity = "Coding"
+animal = "Elephant"
+print(" My name:", name,"\n","My favorite color:", color,"\n","My favorite activity:", activity,"\n", "My favorite animal:", animal)
+print(f" My name:, {name}","\n",f"My favorite color:, {color}","\n",f"My favorite activity:, {activity}","\n", f"My favorite animal:, {animal}")
+print(f"My name: {name}\nMy favorite color: {color}\nMy favorite activity: {activity}\nMy favorite animal:, {animal}")
+
+
 
