3075cf48e45039a8b45dba1a91c1151fe39b3c87
pfb2024 <pfb2024@info19.cshl.edu>
2024-10-15 12:18:09 -0400

    updated file

---
+++ aboutmesysargv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+print ('My name is ', name,'\n', 'My favorite color is ', color,'\n', 'My favorite activity is ', activity, '\n', 'My favorite animal is ', animal,sep="")
