ed43a0782ac964b38e97ca2bd3436ca893fccbbb
pfb2024 <pfb2024@info19.cshl.edu>
2024-10-17 10:46:32 -0400

    q1

---
+++ p5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+Dict = {'Book': "The Catcher in the Rye" , 'Song': "Eminem - Mockingbird" , 'Tree': "Blue Mimosa"}
+print(Dict)
+print(Dict['Book'])
+fav_thing = 'Book'
+print(Dict[fav_thing])
+print(Dict['Tree'])
+Dict['Organism']= "Moon Jellyfish"
+print(Dict)
+
