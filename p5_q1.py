d6c8e1aa02eb0d14b53afb002c391f6c2588841b
pfb2024 <pfb2024@info19.cshl.edu>
2024-10-17 11:44:09 -0400

    q1-9 except 7 and 10

---
+++ p5_q1.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+import sys
+
+#question number 1
+Dict = {'Book': "The Catcher in the Rye" , 'Song': "Eminem - Mockingbird" , 'Tree': "Blue Mimosa"}
+print(Dict)
+
+#question number 2
+print(Dict['Book'])
+
+#question number 3
+fav_thing = 'Book'
+print(Dict[fav_thing])
+
+#question number 4
+print(Dict['Tree'])
+
+#guestion number 5
+Dict['Organism']= "Moon Jellyfish"
+print(Dict)
+
+#question number 6
+for thing in Dict:
+    print(thing, Dict[thing])
+
+#question number none
+fav_thing = Dict.copy()
+for thing in fav_thing:
+    print( f"{thing:<10}      {fav_thing[thing]}")
+
+
+#question number 8
+print(fav_thing)
+thing = input()
+print(Dict[thing])
+
+#question number 9
+fav_thing['Organism'] = 'Unicorn'
+print(fav_thing)
